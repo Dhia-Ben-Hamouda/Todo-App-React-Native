ac32c4e3963d3de250ff9bb5b73f33fe67f339fd
Dhia Ben Hamouda <dhiabenhamouda.5@gmail.com>
2022-08-30 20:03:13 +0100

    working on sign up page

--- screens/SignUp.jsx
+++ screens/SignUp.jsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { View } from "react-native";
+import { View , Text , TextInput } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { useEffect } from "react";
+import { FontAwesome5 } from "@expo/vector-icons";
 
 export default function () {
   const navigation = useNavigation();
@@ -17,6 +18,50 @@ export default function () {
       <View className="h-[130px] w-full relative" >
         <View className="bg-[#00B0FF] h-[260px] w-[260px] rounded-full absolute top-[-130px] left-[-130px]  ">
 
+        </View>
+      </View>
+      <View className="items-center" >
+        <Text className="text-xl font-bold text-[#777] mb-4" >
+          Welcome Onboard !
+        </Text>
+        <Text className="text-lg text-[#777] mb-8" >
+          Let's help you manage your tasks
+        </Text>
+        <View className="w-full px-8" >
+          <View className="flex flex-row bg-white my-4 p-4 px-8 rounded-full align-center" >
+            <FontAwesome5 name="signature" size={24} color="#777" />
+            <TextInput 
+              placeholder="Enter name..."
+              className="flex-1 ml-3"
+            />
+          </View>
+        </View>
+        <View className="w-full px-8" >
+          <View className="flex flex-row bg-white mb-2 p-4 px-8 rounded-full align-center" >
+            <FontAwesome5 name="phone-alt" size={24} color="#777" />
+            <TextInput 
+              placeholder="Enter phone..."
+              className="flex-1 ml-3"
+            />
+          </View>
+        </View>
+        <View className="w-full px-8" >
+          <View className="flex flex-row bg-white my-2 p-4 px-8 rounded-full align-center" >
+            <FontAwesome5 name="user-alt" size={24} color="#777" />
+            <TextInput 
+              placeholder="Enter email..."
+              className="flex-1 ml-3"
+            />
+          </View>
+        </View>
+        <View className="w-full px-8" >
+          <View className="flex flex-row bg-white my-2 p-4 px-8 rounded-full align-center" >
+            <FontAwesome5 name="lock" size={24} color="#777" />
+            <TextInput 
+              placeholder="Enter password..."
+              className="flex-1 ml-3"
+            />
+          </View>
         </View>
       </View>
     </>
